c648ca9baddc83aef98e412f98db82e3454ffe76
Ubuntu <ubuntu@master.capstoneproject.com>
2021-06-27 16:23:41 +0000

    modified source code

--- application-code/src/main/java/com/easypay/CapstoneProject/CapstoneProjectApplication.java
+++ application-code/src/main/java/com/easypay/CapstoneProject/CapstoneProjectApplication.java
@@ -2,10 +2,17 @@ package com.easypay.CapstoneProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CapstoneProjectApplication {
 
+        @RequestMapping("/")
+	public String home() {
+	            return "Hello Simplilearn! Thanks for the great DevOps course! this is just for the Capstone Project ";
+	              }
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneProjectApplication.class, args);
 	}
