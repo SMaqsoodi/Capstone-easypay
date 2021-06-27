0035704479ddbb0e26cb75b44b6c79fa37e94b63
SMaqsoodi <73438087+SMaqsoodi@users.noreply.github.com>
2021-06-27 11:25:16 -0700

    Update CapstoneProjectApplication.java
    modified source code

--- application-code/src/main/java/com/easypay/CapstoneProject/CapstoneProjectApplication.java
+++ application-code/src/main/java/com/easypay/CapstoneProject/CapstoneProjectApplication.java
@@ -2,7 +2,11 @@ package com.easypay.CapstoneProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
+import org.springframework.web.bind.annotation.GetMapping;
+=======
 import org.springframework.web.bind.annotation.RequestMapping;
+>>>>>>> a9a2d2b4459c365ed3b1066ddc047ba4285f7217
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,4 +21,35 @@ public class CapstoneProjectApplication {
 		SpringApplication.run(CapstoneProjectApplication.class, args);
 	}
 
+	@RestController
+	static final class MyController {
+
+		@GetMapping("/")
+		String hello() {
+			return "Hello Simplilearn!\n";
+		}
+	}
+
 }
+
+
+//package com.easypay.CapstoneProject;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+
+//@SpringBootApplication
+//@RestController
+//public class CapstoneProjectApplication {
+
+//        @RequestMapping("/")
+//	public String home() {
+//	            return "Hello Simplilearn! Thanks for the great DevOps course! this is just for the Capstone Project ";
+//	              }
+//	public static void main(String[] args) {
+//		SpringApplication.run(CapstoneProjectApplication.class, args);
+//	}
+
+//}
